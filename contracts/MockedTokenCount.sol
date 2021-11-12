836a796338151301b6cbdca1dda8629ef097ae0c
Mikołaj Tęcza <mikolaj.tecza@rumblefish.pl>
2021-11-12 14:08:30 +0100

    Changed function name from library TokenCount

--- contracts/MockedTokenCount.sol
+++ contracts/MockedTokenCount.sol
@@ -4,7 +4,7 @@ import "../contracts/TokenCount.sol";
 
 contract MockedTokenExchange{
     function mockCount(uint _amount, uint _price, uint _decimals, bool _isBuy, uint _tokenADecimals, uint _tokenBDecimals) public pure returns(uint){
-        uint tokens = TokenCount.count(_amount, _price, _decimals, _isBuy, _tokenADecimals, _tokenBDecimals);
+        uint tokens = TokenCount.countExchangedTokens(_amount, _price, _decimals, _isBuy, _tokenADecimals, _tokenBDecimals);
 
         return tokens;
     }
